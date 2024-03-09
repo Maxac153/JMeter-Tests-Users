a8255837f9532dd162bbf2fe9e55043e7aaf01c3
turgor <maxac153@gmail.com>
2024-03-09 18:44:30 +0500

    refactoring test auth

--- pre_generate_users.py
+++ pre_generate_users.py
@@ -8,17 +8,17 @@ fake = Faker()
 class RegisterUser:
     @staticmethod
     def random_user() -> str:
-        name = fake.name()
+        name = fake.name().replace(" ", "_")
         email = fake.email()
         password = fake.password()
-        return f'{name},{email},{password}\n'
+        return f'user_name={name},email={email},password={password}\n'
 
 
 if __name__ == '__main__':
     print('Start pre gen users')
 
     # Запись данных в CSV файл
-    for _ in range(0, 1000):
+    for _ in range(0, 100000):
         requests.post(f'http://17.150.0.103:9191/sts/ADD?FILENAME=users_data.csv&LINE={RegisterUser.random_user()}&ADD_MODE=LAST&UNIQUE=FALSE')
 
     print('End pre gen users')
