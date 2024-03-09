bf3ead834662b7a64ddea90ea30d09caec33c49d
turgor <maxac153@gmail.com>
2024-03-09 22:33:06 +0500

    refactoring test reg

--- pre_generate_users.py
+++ pre_generate_users.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     print('Start pre gen users')
 
     # Запись данных в CSV файл
-    for _ in range(0, 100000):
+    for _ in range(0, 10000):
         requests.post(f'http://17.150.0.103:9191/sts/ADD?FILENAME=users_data.csv&LINE={RegisterUser.random_user()}&ADD_MODE=LAST&UNIQUE=FALSE')
 
     print('End pre gen users')
